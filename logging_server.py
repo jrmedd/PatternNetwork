b71380bd9e87725aabc00dcd949069254b0a9278
jrmedd <hello@jamesmedd.co.uk>
2017-04-12 17:15:39 +0100

    Change files, add logging server stuff

---
+++ logging_server.py
@@ -0,0 +1,40 @@
+from flask import Flask, flash, url_for, render_template, request, redirect, make_response, Response, jsonify
+from pymongo import MongoClient
+import datetime
+logging = False
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client['pattern_network']
+patterns = db['patterns']
+
+app = Flask(__name__)
+
+app.secret_key = "L\x97\xb9^\xa4\x18\x91\xd5\xb4\xae\x87\x92&\xf32\xdf\xbfC\xf7S~\xdb)g"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<user_id>/<pattern>')
+def payment(user_id,pattern):
+    details = {'timestamp':datetime.datetime.now()}
+    details.update({'user_id':user_id})
+    details.update({'pattern':pattern})
+    patterns.update({'pattern':pattern}, details, upsert=True)
+    print details
+    return "LOGGED"
+
+@app.route('/db_check')
+def db_check():
+    return jsonify(logging=logging)
+
+@app.route('/recent')
+def recent():
+    recent = list(patterns.find({'timestamp':{'$gt':datetime.datetime.now() - datetime.timedelta(seconds=3)}},{'_id':0}))
+    if len(recent) > 0:
+        return jsonify(result=recent[0])
+    else:
+        return jsonify(result=None)
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",debug=True)
--- pattern_network.py
+++ pattern_network.py
@@ -1,19 +1,32 @@
 import serial
 import OSC
-
+import requests
+#the serial connection (default /dev/ttyACM0 on Pi)
 pc = serial.Serial('/dev/ttyACM0', 115200)
-
+#OSC client object
 client = OSC.OSCClient()
+#set a UDP port to send OSC messages
 my_port = int(raw_input("What UDP port shall I connect with?\n"))
+#set a user ID (0-8) so we know what instrument to control
 my_user = raw_input("What shall my user number be?\n")
+#create an OSC address based on that
 my_address = "/user/" + my_user
+#set the IP of the 'brain' computer, which will receive all patterns and create music
 brain_ip = raw_input("...and what is the brain's IP?\n")
+#set url for DB logging
+brain_url = 'http://%s:5000/' % (brain_ip)
+#attempt OSC connection
 try:
     client.connect((brain_ip, my_port))
-    print "Okay, I've connected to %s, using port %d, and I'm user %s" % (brain_ip, my_port, my_user)
+    print "Okay, I've connected via UDP to %s, using port %d, and I'm user %s" % (brain_ip, my_port, my_user)
 except:
-    print "Failed to connect"
+    print "Failed to connect via UDP"
 
+try:
+    db_check = requests.get(brain_url + "db_check")
+    logging = db_check.json().get('logging')
+except:
+    logging = False
 print "Now awaiting cards!"
 while True:
     card = pc.readline()
